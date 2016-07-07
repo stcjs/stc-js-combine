a008ba8b7c6cb81216e1215b4fceb0dda358301f
zhangjudong <zhangjudong@360.cn>
2016-07-07 15:54:37 +0800

    edit

--- src/index.js
+++ src/index.js
@@ -1,70 +1,125 @@
 import Plugin from 'stc-plugin';
 
 const REG = {
-    COMMENTS: /((^|[^"':\s])\/\/.*$)|((^|[^"':\s])\/\*(.|\s)*?\*\/)/g,
-    DOCUMENT_WRITE: /document\.write.*?srcPath.*?['"](.*?)['"].*?<\/script>/g,
+    COMMENTS: /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)/g,
+    DOCUMENT_WRITE: /document\.write.*?srcPath.*?['"](.*?)['"]/g,
+    SCRIPT_NAME: /srcPath.*?['"](.*?)['"]/,
     SRCPATH: /var\s+srcPath\s*=\s*("|')(.*?)\1/
 };
 const HANDLEDRFILES = 'handledFiles';
-
-
-console.log(Plugin.prototype.cache);
-for(var key in Plugin.prototype) {
-    console.log(key);
-
-}
+const FILESTREE = 'filesTree';
 
 export default class JSCombinePlugin extends Plugin {
+    log(...args) {
+        return;
+        console.log(this.file.path, ':', args);
+    }
+    async getContentFormCache(filePath) {
+        let filesTree = await this.cache(FILESTREE);
+        filesTree = filesTree || {};
+        let arr  = [];
+        function combine(p) {
+            if (filesTree[p]) {
+                filesTree[p].forEach((v) => {
+                    combine(v);
+                });
+            } else {
+                if (arr.indexOf(p) == -1){
+                    arr.push(p);
+                }
+            }
+        }
+        combine(filePath);
+        let content = [];
+        let promise = arr.map(async (v) =>{
+            let cache = await this.cache(v);
+            if (cache) {
+                content.push(cache);
+            }
+        })
+        await Promise.all(promise);
+        content = content.join(';')
+        await this.cache(filePath, content);
+        return content;
+    }
     /**
      * run
      */
     async run(){
-        // console.log(this.constructor.name, this.getContent, this.getAst);
-        // console.log(this.cache);
-         return;
-        let content, pathPreRes, pathPre, scipts, newFileContent = [], curHandledFiles = {}, handledFiles = await this.cache(HANDLEDRFILES);
+        let stcFilePath = this.file.path;
+        if (stcFilePath.indexOf('/') != 0) {
+            stcFilePath = '/' + stcFilePath;
+        }
+        this.log('enter ------------------------>');
+        //如果有缓存，直接返回
+        let content = await this.cache(stcFilePath);
+        if (content) {
+            return content;
+        }
         content = await this.getContent('utf8');
         //去掉注释
-        content = content.replace(REG.COMMENTS, '');
-        //得到公共路径
-        pathPreRes = REG.SRCPATH.exec(content);
-        if (pathPreRes || !pathPreRes[2]) {
-            pathPre = pathPre[2] + '/';
-            scripts = content.match(REG.DOCUMENT_WRITE);
-            if (scripts && scripts.length) {
-                let promise = scripts.map(async (str) => {
-                    let filePath,  fileContent, fileName, res;
-                    res = REG.DOCUMENT_WRITE.exec(str);
-                    if (!res || !res[1]){
-                        return;
-                    }
-                    fileName = res[1];
-                    filePath = pathPre + fileName;
-                    if (!curHandledFiles[filePath]) {
-                        if (handledFiles[filePath]) {
-                            newFileContent.push(handledFiles[filePath]);
-                            curHandledFiles[filePath] = true;
-                        } else {
-                            fileContent = await this.invokeSelf(this.getFileByPath(filePath));
-                            newFileContent.push(fileContent);
-                            handledFiles[filePath] =  fileContent;
-                            curHandledFiles[filePath] = true;
-                            this.cache(HANDLEDRFILES, handledFiles);
-                        }
-                    }
-                });
-                await Promise.all(promise);
-            }
+        content = content.replace(REG.COMMENTS, (word) =>{
+            return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word;
+        });
+        //获取公共路径
+        let srcPathRes = REG.SRCPATH.exec(content);
+        if (!srcPathRes || !srcPathRes[2]) {
+            return content;
         }
-        return newFileContent.length ? newFileContent.join(';') : content;
+        let srcPath = srcPathRes[2];
+        this.log('commen path: ', srcPath);
+        //获取document.write script
+        let scripts = content.match(REG.DOCUMENT_WRITE);
+        if (!scripts || !scripts.length) {
+            return content;
+        }
+        //获取script路径
+        let scriptPathes = [];
+        scripts.forEach((str) => {
+            let res = REG.SCRIPT_NAME.exec(str);
+            if (res && res[1]){
+                let jsPath = srcPath + res[1];
+                if (scriptPathes.indexOf(jsPath) == -1) {
+                    scriptPathes.push(jsPath);
+                }
+            }
+        });
+        if (!scriptPathes.length) {
+            return content;
+        }
+        //缓存文件关系
+        let filesTree = await this.cache(FILESTREE);
+        filesTree = filesTree || {};
+        if (!filesTree[stcFilePath]){
+            filesTree[stcFilePath] = scriptPathes;
+            await this.cache(FILESTREE, filesTree);
+        }
+        this.log('scripts: ', scriptPathes);
+        //获取文件内容并缓存
+        let promise = scriptPathes.map(async (filePath) => {
+            let fileContent = await this.cache(filePath);
+            if (fileContent) {
+                return;
+            }
+            let stcFile = await this.getFileByPath(filePath);
+            fileContent = await this.invokeSelf(stcFile);
+            await this.cache(filePath, fileContent);
+        });
+        await Promise.all(promise);
+        content = await this.getContentFormCache(stcFilePath);
+        return content;
     }
 
     /**
      * update
      */
     update(str){
-
-        //this.setContent(str);
+        str = str.trimRight();
+        if (str.charAt(str.length-1) != ';') {
+            str += ';'
+        }
+        this.log('content: ', str, '<--------end');
+        this.setContent(str);
     }
 
     /**
